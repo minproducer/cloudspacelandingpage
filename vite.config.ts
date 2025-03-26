a11da640f6a5e4b42b0e016ec5eb2aec4e608109
hmtravel <carshoian@gmail.com>
2025-03-27 01:51:45 +0700

    fix issue

--- vite.config.ts
+++ vite.config.ts
@@ -30,8 +30,8 @@ export default defineConfig({
   },
   root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "dist"),
+    outDir: path.resolve(__dirname, "dist/public"),
     emptyOutDir: true,
   },
-  base: '/cloudspacelandingpage',
+  base: '/cloudspacelandingpage/',
 });
